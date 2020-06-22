5ed0e4d4883f099f404663c161786bd33613aea4
andrii.kovalchuk <shtramak@gmail.com>
2020-06-22 14:43:49 +0300

    add spring-boot project with basic preparations

---
+++ src/test/java/com/shtramak/springsecuritytraining/SpringSecurityTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shtramak.springsecuritytraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
